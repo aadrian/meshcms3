f9b9ea58bfa7825bd7636461162c63db618da72d
amohombe <amohombe@1cc7fbaf-6d0f-0410-9def-d668455538ed>
2006-05-27 17:41:51 +0000

    Added initial Unit Test support. This is only the infrastructure, and contains no real unit tests so far (the still must be written).

---
+++ meshcms3/src/test/org/meshcms/core/WebSiteTest.java
@@ -0,0 +1,52 @@
+package org.meshcms.core;
+
+import junit.framework.*;
+import org.meshcms.core.WebSite;
+
+public class WebSiteTest extends TestCase {
+    WebSite webSite;
+
+    public void testGetStatsIndex() throws Exception {
+        fail("Test is not implemented");
+    }
+
+    public void testCreateDirectory() throws Exception {
+        fail("Test is not implemented");
+    }
+
+    public void testCreateFile() throws Exception {
+        fail("Test is not implemented");
+    }
+
+    public void testSaveToFile() throws Exception {
+        fail("Test is not implemented");
+    }
+
+    public void testGetLinkList() throws Exception {
+        fail("Test is not implemented");
+    }
+
+    public void testIsSystem() throws Exception {
+        fail("Test is not implemented");
+    }
+
+    public void testGetModulePath() throws Exception {
+        fail("Test is not implemented");
+    }
+
+    public void testGetHTMLTemplate() throws Exception {
+        fail("Test is not implemented");
+    }
+
+    public void testFindCurrentWelcome() throws Exception {
+        fail("Test is not implemented");
+    }
+
+    public void testLoadFromXML() throws Exception {
+        fail("Test is not implemented");
+    }
+
+    public void testStoreToXML() throws Exception {
+        fail("Test is not implemented");
+    }
+}
---
+++ meshcms3/src/test/org/meshcms/core/WebUtilsTest.java
@@ -0,0 +1,68 @@
+package org.meshcms.core;
+
+import junit.framework.*;
+import org.meshcms.core.WebUtils;
+
+public class WebUtilsTest extends TestCase {
+    WebUtils webUtils;
+
+    public void testConvertToHTMLEntities() throws Exception {
+        fail("Test is not implemented");
+    }
+
+    public void testGetWelcomeFiles() throws Exception {
+        fail("Test is not implemented");
+    }
+
+    public void testGetFullParameter() throws Exception {
+        fail("Test is not implemented");
+    }
+
+    public void testGetFullRequestURL() throws Exception {
+        fail("Test is not implemented");
+    }
+
+    public void testGetContextHomeURL() throws Exception {
+        fail("Test is not implemented");
+    }
+
+    public void testGetFullThemeFolder() throws Exception {
+        fail("Test is not implemented");
+    }
+
+    public void testGetFullThemeFile() throws Exception {
+        fail("Test is not implemented");
+    }
+
+    public void testGetFullThemeCSS() throws Exception {
+        fail("Test is not implemented");
+    }
+
+    public void testGetFullMeshCSS() throws Exception {
+        fail("Test is not implemented");
+    }
+
+    public void testGetMenuCode() throws Exception {
+        fail("Test is not implemented");
+    }
+
+    public void testGet2ndLevelDomain() throws Exception {
+        fail("Test is not implemented");
+    }
+
+    public void testFixFileName() throws Exception {
+        fail("Test is not implemented");
+    }
+
+    public void testFormatFileLength() throws Exception {
+        fail("Test is not implemented");
+    }
+
+    public void testGetCharsetCanonicalName() throws Exception {
+        fail("Test is not implemented");
+    }
+
+    public void testParseCharset() throws Exception {
+        fail("Test is not implemented");
+    }
+}
