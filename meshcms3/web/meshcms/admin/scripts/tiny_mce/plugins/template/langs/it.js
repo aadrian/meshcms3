150b85f87904ed79074079f396ad319db025fb28
lvplanet <lvplanet@1cc7fbaf-6d0f-0410-9def-d668455538ed>
2007-03-01 13:46:05 +0000

    Update TinyMCE Italian translation

---
+++ meshcms3/web/meshcms/admin/scripts/tiny_mce/plugins/template/langs/it.js
@@ -0,0 +1,22 @@
+/**
+ * IT lang variables
+ * 
+ * Author : Luciano Vernaschi <luciano@virgilio.it>
+ * Last Updated : Mar. 1st, 2007
+ * TinyMCE Version : 2.1.0
+ */
+
+tinyMCE.addToLang('template',{
+title : 'Modelli',
+label : 'Modello',
+desc_label : 'Descrizione',
+desc : 'Inserisci contenuto predefinito del modello',
+select : 'Seleziona un modello',
+preview : 'Anteprima',
+warning : 'Attenzione: aggiornare un template con un altro pu&ograve; causare una perdita di dati',
+def_date_format : '%Y-%m-%d %H:%M:%S',
+months_long : new Array("Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno", "Luglio", "Agosto", "Settembre", "Ottobre", "Novembre", "Dicembre"),
+months_short : new Array("Gen", "Feb", "Mar", "Apr", "Mag", "Giu", "Lug", "Ago", "Set", "Ott", "Nov", "Dic"),
+day_long : new Array("Domenica", "Luned&igrave;", "Marted&igrave;", "Mercoled&igrave;", "Gioved&igrave;", "Venerd&igrave;", "Sabato", "Domenica"),
+day_short : new Array("Dom", "Lun", "Mar", "Mer", "Gio", "Ven", "Sab", "Dom")
+});
