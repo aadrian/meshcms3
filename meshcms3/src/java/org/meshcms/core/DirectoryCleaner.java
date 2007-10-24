cc544c621062b97ac4c54839f12bda0cb2ad9b1f
lvplanet <lvplanet@1cc7fbaf-6d0f-0410-9def-d668455538ed>
2007-10-24 15:29:30 +0000

    Added a constructor to use this cleaner just to remove empty dirs without touching any file

--- meshcms3/src/java/org/meshcms/core/DirectoryCleaner.java
+++ meshcms3/src/java/org/meshcms/core/DirectoryCleaner.java
@@ -30,52 +30,67 @@ import org.meshcms.util.*;
  */
 public class DirectoryCleaner extends DirectoryParser {
   private long currentTime;
-  private long maxLife;
-
+  private long maxLifeMillis;
+  
   /**
-   * Creates a repository cleaner for the given web application.
+   * Creates a directory cleaner for the given web application to
+   * remove empty directories.
+   */
+  public DirectoryCleaner(File directory) {
+    this(directory, 0L);
+  }
+  
+  /**
+   * Creates a directory cleaner for the given web application to
+   * remove old files empty directories.
+   *
+   * @param maxLifeMillis maximum file age in milliseconds
    */
   public DirectoryCleaner(File directory, long maxLifeMillis) {
     setInitialDir(directory);
-    maxLife = maxLifeMillis;
+    this.maxLifeMillis = maxLifeMillis;
     setRecursive(true);
     setDaemon(true);
     setName("DirectoryCleaner for \"" + Utils.getFilePath(directory) + '"');
     setPriority(Thread.MIN_PRIORITY);
   }
-
+  
   protected boolean preProcess() {
     currentTime = System.currentTimeMillis();
     return true;
   }
-
+  
   protected void postProcessDirectory(File file, Path path) {
     // try to delete the folder: this will succeed if the folder is empty
     // so we can get rid of useless folders.
     file.delete();
   }
-
+  
   protected void processFile(File file, Path path) {
-    String name = file.getName();
-
-    // process files whose file name is in the form
-    // something_lastmodified.extension
-    // for example _bak_admin_20060128093159.html is a backup of a page
-    int us = name.lastIndexOf('_');
-
-    if (us >= 0) {
-      try {
-        if (currentTime - WebUtils.numericDateFormatter.parse(name.substring(us + 1, us + 15)).getTime() > maxLife) {
-          Utils.forceDelete(file);
-        }
-
-        return;
-      } catch (Exception ex) {}
-    }
-
-    // other files are deleted when too old based on last modified date.
-    if (currentTime - file.lastModified() > (long) (maxLife * (1.0 + Math.random() / 2.0))) {
-      Utils.forceDelete(file);
+    if (maxLifeMillis > 0L) {
+      String name = file.getName();
+      
+      // process files whose file name is in the form
+      // something_lastmodified.extension
+      // for example _bak_admin_20060128093159.html is a backup of a page
+      int us = name.lastIndexOf('_');
+      
+      if (us >= 0) {
+        try {
+          if (currentTime - WebUtils.numericDateFormatter.parse
+              (name.substring(us + 1, us + 15)).getTime() > maxLifeMillis) {
+            Utils.forceDelete(file);
+          }
+          
+          return;
+        } catch (Exception ex) {}
+      }
+      
+      // other files are deleted when too old based on last modified date.
+      if (currentTime - file.lastModified() >
+          (long) (maxLifeMillis * (1.0 + Math.random() / 2.0))) {
+        Utils.forceDelete(file);
+      }
     }
   }
 }
