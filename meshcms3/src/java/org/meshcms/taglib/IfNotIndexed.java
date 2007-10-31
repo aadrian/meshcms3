ef49a48e3cb987f400b6e153ca14101f49f1584b
lvplanet <lvplanet@1cc7fbaf-6d0f-0410-9def-d668455538ed>
2007-10-31 10:26:49 +0000

    Added some simple tags to check if a page is indexed (i.e. it is in the site map) or if it is an admin page (useful when the system theme is not used)

---
+++ meshcms3/src/java/org/meshcms/taglib/IfNotIndexed.java
@@ -0,0 +1,40 @@
+/*
+ * MeshCMS - A simple CMS based on SiteMesh
+ * Copyright (C) 2004-2007 Luciano Vernaschi
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * You can contact the author at http://www.cromoteca.com
+ * and at info@cromoteca.com
+ */
+
+package org.meshcms.taglib;
+
+import java.io.*;
+
+/**
+ * This tag allows its contents to be written only when the current page is
+ * being edited.
+ */
+public class IfNotIndexed extends AbstractTag {
+  public void writeTag() throws IOException {
+    // nothing to do here
+  }
+  
+  public int getStartTagReturnValue() {
+    return webSite.getSiteMap().getPageInfo(pagePath) == null ?
+      EVAL_BODY_INCLUDE : SKIP_BODY;
+  }
+}
